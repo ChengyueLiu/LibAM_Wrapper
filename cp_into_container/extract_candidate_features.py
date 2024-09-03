c8ccd90106450997df207d133f5da80aa0d361fa
ChengyueLiu <17554334529@163.com>
2024-09-03 19:11:33 +0800

    optimize candidate feature extraction

--- cp_into_container/extract_candidate_features.py
+++ cp_into_container/extract_candidate_features.py
@@ -1,3 +1,4 @@
+import shutil
 import sys, os
 from settings import *
 
@@ -13,18 +14,24 @@ import binary_preprocess as binary_preprocess_module
 
 
 def cli():
-
     print("start generate candidate feature......")
 
-    # candidate 信息文件夹
+    # candidate dir
     candidate_dir = os.path.join(DATA_PATH, "candidate")
 
-    # 子目录
+    # children dir
     time_cost_dir = os.path.join(candidate_dir, "timecost")
     candidate_binary_dir_path = os.path.join(candidate_dir, "binaries")
     candidate_feature_dir_path = os.path.join(candidate_dir, "features")
 
-    # 提取特征
+    # clean the candidate_feature_dir_path, if not clean, these dir ends with "_" will cause errors.
+    if os.path.exists(candidate_binary_dir_path):
+        for item in os.listdir(candidate_binary_dir_path):
+            item_path = os.path.join(candidate_binary_dir_path, item)
+            if item.endswith("_") and os.path.isdir(item_path):
+                shutil.rmtree(item_path, ignore_errors=True)
+
+    # extract feature
     binary_preprocess_module.getAllFiles(time_cost_dir,
                                          candidate_binary_dir_path,
                                          candidate_feature_dir_path, mode="1")
