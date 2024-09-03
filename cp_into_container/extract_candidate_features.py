d1c208f9302c915250e423d6bab7ffddb23adce6
ChengyueLiu <17554334529@163.com>
2024-09-03 19:06:24 +0800

    optimize candidate feature extraction

--- cp_into_container/extract_candidate_features.py
+++ cp_into_container/extract_candidate_features.py
@@ -1,5 +1,4 @@
 import sys, os
-# import click
 from settings import *
 
 sys.path.append("code/anchor_detection/semantic_anchor_detection")
@@ -10,16 +9,7 @@ sys.path.append("code/reuse_area_exploration/Embeded-GNN")
 sys.path.append("code/reuse_area_exploration/TPL_detection")
 sys.path.append("code/reuse_area_exploration/reuse_area_detection")
 
-import all_func_compare_isrd as anchor_detection_module
 import binary_preprocess as binary_preprocess_module
-import Generate_func_embedding as embeddings_generate_module
-import get_tainted_graph as anchor_reinforcement_module
-import fcg_gnn_score as embeded_gnn_module
-import get_final_score_multi as TPL_detection_module1
-import get_final_result_dict as TPL_detection_module2
-import cal_result as TPL_detection_module3
-import adjust_area as area_adjustment_module
-import compare_area as reuse_area_detection_module
 
 
 def cli():
